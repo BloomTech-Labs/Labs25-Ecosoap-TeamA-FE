da25486d0ddee4aed25fbd93daf61ee1e88c1d9d
DeLaMoraRodrigo <delamorarodrigo3141@yahoo.com>
2020-09-20 03:19:22 -0700

    Made it so that when you delete a field on a type that all the records under that type have the matching field deleted.

--- src/components/fields/TypeFieldsCard.js
+++ src/components/fields/TypeFieldsCard.js
@@ -153,8 +153,147 @@ function TypeFieldsCard(props) {
 
     props.setTableState(!props.tableState);
   }
-  function delField(id) {
-    console.log('You really gonna delete me', id);
+  async function delField(id) {
+    // console.log('You really gonna delete me', id);
+
+    let fixedFields = props.type.fields.map(field => {
+      delete field.__typename;
+      delete field.id;
+      return field;
+    });
+
+    console.log('OLD FIELDS', fixedFields);
+
+    let updatedFields = inspect(
+      fixedFields.filter(field => {
+        return field.name !== props.field.name;
+      })
+    )
+      .split("'")
+      .join('"');
+
+    // console.log('UPDATED FIELDS', updatedFields);
+
+    let DELETE_TYPE_FIELD_MUTATION = gql`
+        mutation {
+            updateType(input: {id: "${props.type.id}" name: "${props.type.name}", icon: "${props.type.icon}", fields: ${updatedFields}}){
+            type{
+                id,
+                name,
+                icon,
+                fields{
+                name,
+                value
+                }
+            }
+            }
+        }
+    `;
+
+    await client
+      .mutate({ mutation: DELETE_TYPE_FIELD_MUTATION })
+      .then(async res => {
+        console.log('DELETE TYPE FIELD RES', res);
+
+        let batchArray = [];
+        let counter = 0;
+
+        await props.recordsState.data.recordsByType.map(async record => {
+          let fixedRecordFields = await record.fields.map(field => {
+            delete field.id;
+            delete field.__typename;
+            return field;
+          });
+
+          let replacedFields = inspect(
+            fixedRecordFields.filter(field => {
+              return field.name !== props.field.name;
+            })
+          )
+            .split("'")
+            .join('"');
+
+          let BATCH_QUERY = `mutation${counter}: updateRecord(
+            input: {
+              id: "${record.id}"
+              name: "${record.name}"
+              coordinates: { latitude: ${record.coordinates.latitude}, longitude: ${record.coordinates.longitude} }
+              fields: ${replacedFields}
+            }
+          ) {
+            record {
+              id
+              name
+              coordinates {
+                latitude
+                longitude
+              }
+              fields {
+                id
+                name
+                value
+              }
+            }
+          }`;
+
+          batchArray.push(BATCH_QUERY);
+          counter += 1;
+        });
+
+        let gqlString = `mutation {${batchArray}}`;
+
+        let batchMutation = gql`
+          ${gqlString}
+        `;
+
+        await client
+          .mutate({ mutation: batchMutation })
+          .then(res => {
+            console.log('DELETED RECORD FIELD RESPONSE: ', res);
+          })
+          .catch(err => {
+            console.log('ERROR', err);
+          });
+      })
+      .catch(err => {
+        console.log('ERROR', err);
+      });
+
+    await client
+      .query({ query: FETCH_TYPES })
+      .then(res => {
+        console.log('FETCH TYPES RES', res);
+        props.setTypes(res.data.types);
+      })
+      .catch(err => {
+        console.log('ERROR', err);
+      });
+
+    let GET_TYPE = gql`
+      {
+        typeById(input: {typeId: "${props.type.id}"}){
+          id
+          name
+          icon
+          fields {
+            name
+            value
+          }
+        }
+      }
+      `;
+
+    await client
+      .query({ query: GET_TYPE })
+      .then(res => {
+        console.log('TYPE', res);
+        props.setType(res.data.typeById);
+      })
+      .catch(err => {
+        console.log('ERROR', err);
+      });
+
+    props.setTableState(!props.tableState);
   }
   return (
     <div className="fieldsCard">
